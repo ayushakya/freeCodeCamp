78ee548c08650ef08d2d174db0a3131d6fb03135
ayushakya <65272892+ayushakya@users.noreply.github.com>
2023-06-04 13:30:07 +0545

    Update element.sh

--- RDBMS/Periodic Table/element.sh
+++ RDBMS/Periodic Table/element.sh
@@ -15,13 +15,20 @@ then
   ELEMENT=$($PSQL "SELECT atomic_number, symbol, name, type, atomic_mass, melting_point_celsius, boiling_point_celsius FROM elements JOIN properties USING(atomic_number) JOIN types USING(type_id) WHERE atomic_number=$1")
   # echo $ELEMENT
 else
-  # symbol or name
+  # symbol or name (string)
   ELEMENT=$($PSQL "SELECT atomic_number, symbol, name, type, atomic_mass, melting_point_celsius, boiling_point_celsius FROM elements JOIN properties USING(atomic_number) JOIN types USING(type_id) WHERE name = '$1' OR symbol = '$1'")
   # echo $ELEMENT
 fi
 
-# if not found
+# if no record
 if [[ -z $ELEMENT ]]
 then
-  echo -e "\nI could not find that element in the database.\n"
+  echo I could not find that element in the database.
+  exit
 fi
+
+# display output
+echo $ELEMENT | while IFS=" |" read an symbol name type mass mp bp 
+do
+  echo -e "The element with atomic number $an is $name ($symbol). It's a $type, with a mass of $mass amu. $name has a melting point of $mp celsius and a boiling point of $bp celsius."
+done
